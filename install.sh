f5825518eb5d8a4eaebd4daec6745581046b2973
georgekcibi <georgycibi@gmail.com>
2023-03-19 20:27:14 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-vdf
+#!/bin/bash
+sudo systemctl stop springboot
+cd /home/ubuntu/application/spring-boot-jsp-jenkins-code-pipeline/target
+sudo rm news-v1.0.4.jar
+cd /home/ubuntu && target/*.jar
+sudo cp target/* /home/ubuntu/application/spring-boot-jsp-jenkins-code-pipeline/target
+sudo systemctl start springboot
